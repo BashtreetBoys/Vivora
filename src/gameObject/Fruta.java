4c0eb2f7287add6a4c21339fe9bce21549fb79d0
AlexSvad <42757079+AlexSvad@users.noreply.github.com>
2018-10-12 15:55:34 -0300

    Add files via upload

--- src/gameObject/Fruta.java
+++ src/gameObject/Fruta.java
@@ -2,4 +2,52 @@ package gameObject;
 
 public class Fruta {
 
+	private String powerUp;
+	private int posX;
+	private int posY;
+	private int numeroFruta;
+	
+	public Fruta(String powerUp, int posX, int posY,int numeroFruta) {
+		this.powerUp = powerUp;
+		this.posX = posX;
+		this.posY = posY;
+		this.numeroFruta=numeroFruta;
+	}
+	
+	public Fruta() {}
+
+	
+	public String getPowerUp() {
+		return powerUp;
+	}
+
+	public void setPowerUp(String powerUp) {
+		this.powerUp = powerUp;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getNumeroFruta() {
+		return numeroFruta;
+	}
+
+	public void setNumeroFruta(int numeroFruta) {
+		this.numeroFruta = numeroFruta;
+	}
+	
+	
 }
