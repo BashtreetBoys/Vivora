35ccf8a44dcfe2ce916583155f070defe69b2cc7
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-12 20:56:49 -0300

    Update src/gameObject/Pruebas/ObstaculoTest.java

--- src/gameObject/Pruebas/ObstaculoTest.java
+++ src/gameObject/Pruebas/ObstaculoTest.java
@@ -1,6 +1,17 @@
 package gameObject.Pruebas;
 
+import org.junit.Test;
+import org.junit.Assert;
+import gameObject.Obstaculo;
 public class ObstaculoTest {
 	
+	@Test
+	public void crearObstaculo() {
+		Obstaculo nuevo = new Obstaculo(1,1,2,2);
+		Assert.assertEquals(1,nuevo.getPosXini());
+		Assert.assertEquals(1,nuevo.getPosYini());
+		Assert.assertEquals(2,nuevo.getPosXfin());
+		Assert.assertEquals(2,nuevo.getPosXfin());
+	}
 
 }
