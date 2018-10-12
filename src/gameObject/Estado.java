c5b12a3612cb974a0eab61082fc13d956939bb30
Matias De Stefano <matiasdestefano97@gmail.com>
2018-10-12 16:28:37 -0300

    Clase Envenenada
    vibora.removerCuerpo() en class Envenenada

--- src/gameObject/Estado.java
+++ src/gameObject/Estado.java
@@ -1,5 +1,9 @@
 package gameObject;
 
 public interface Estado {
+	/**
+	 * Según el estado de la vibora, el método aplicará el Power-Up correspondiente.
+	 * @param vibora Vibora a la cual se le aplicará el Power-Up.
+	 */
 	public void cambiarEstado(Vibora vibora);
 }
