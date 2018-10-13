dda4f48eb7b4ffd6fb63d734cd61eecf21f7012c
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-12 21:46:12 -0300

    Terminado test Jugador, agregado un setter

--- src/gameObject/Pruebas/JugadorTest.java
+++ src/gameObject/Pruebas/JugadorTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.Assert;
 import gameObject.Vibora;
 import gameObject.Jugador;
+import gameObject.Arena;
 
 public class JugadorTest {
 
@@ -11,5 +12,24 @@ public class JugadorTest {
 		Vibora mySnake = new Vibora(1,1);
 		Jugador newPlayer = new Jugador("TheBo55","Listo",mySnake,"TheBo55esPass");
 		Assert.assertEquals(0, newPlayer.getPuntaje());
+		Assert.assertEquals(newPlayer.getNombre(),"TheBo55");
+	}
+	
+	public void prueboViboritaJugador() {
+		Vibora mySnake = new Vibora(1,1);
+		Jugador newPlayer = new Jugador("TheBo55","Listo",mySnake,"TheBo55esPass");
+		Arena a = new Arena();
+		
+		newPlayer.salirDeArena(a);
+		
+		Vibora theBo55Snake = newPlayer.getVivorita();
+		
+/**
+ * Verifico que la vibora dentro del jugador funcione correctamente y pueda acceder a los metodos
+ */
+		Assert.assertEquals(2, theBo55Snake.getVelocidad());
+		theBo55Snake.moverVibora(2);
+		
+		
 	}
 }
