f55df83fcac5d3bbd69dcd9e52bb6402d492e2e3
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-12 21:39:20 -0300

    Update src/gameObject/Pruebas/JugadorTest.java

--- src/gameObject/Pruebas/JugadorTest.java
+++ src/gameObject/Pruebas/JugadorTest.java
@@ -1,5 +1,15 @@
 package gameObject.Pruebas;
+import org.junit.Test;
+import org.junit.Assert;
+import gameObject.Vibora;
+import gameObject.Jugador;
 
 public class JugadorTest {
 
+	@Test
+	public void creoJugador() {
+		Vibora mySnake = new Vibora(1,1);
+		Jugador newPlayer = new Jugador("TheBo55","Listo",mySnake,"TheBo55esPass");
+		Assert.assertEquals(0, newPlayer.getPuntaje());
+	}
 }
